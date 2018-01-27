caaf7729ad25625a5307d9c1e4b0b96319f22864
manu <mcvictor@unicauca.edu.co>
2018-01-27 00:12:58 -0500

    Lista de profesores

---
+++ Horarios/resources/views/horario/profesor/create.blade.php
@@ -0,0 +1,11 @@
+@extends ('layouts.admin')
+
+@section ('contenido')
+	<div class="row">
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+			<h3></h3>
+		</div>
+	</div>
+
+
+@endsection
---
+++ Horarios/resources/views/horario/profesor/index.blade.php
@@ -0,0 +1,44 @@
+@extends ('layouts.admin')
+
+@section ('contenido')
+<div class="row">
+	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+		<h3>Listado de Profesores<a href="profesor/create"><button class="btn btn-success">Nuevo</button></a></h3>
+		@include('horario.profesor.search')
+	</div>
+</div>
+
+
+<div class="row">
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<div class="table-responsive">
+			<table class="table table-striped table-bordered table-condensed table-hover">
+				<thead>
+					<th>Cedula</th>
+					<th>Nombre</th>
+					<th>Telefono</th>
+					<th>Correo</th>
+					<th>Estado</th>	
+					<th>Opciones<th>			
+				</thead>
+				@foreach ($profesores as $pro)
+					<tr>
+						<td>{{ $pro->Cedula}}</td>
+						<td>{{ $pro->Nombre}}</td>
+						<td>{{ $pro->Telefono}}</td>
+						<td>{{ $pro->Correo}}</td>
+						<td>{{ $pro->Estado}}</td>	
+						<td>
+							<a href=""><button class="btn  btn-info"> Editar</button></a>
+
+							<a href=""><button class="btn  btn-danger"> Eliminar </button></a>
+						</td>			
+					</tr>
+				@endforeach
+			</table>		
+		</div>
+		{{ $profesores->render()}}
+	</div>
+</div>
+
+@endsection 
